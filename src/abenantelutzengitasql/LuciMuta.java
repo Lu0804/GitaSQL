5f8f27455fc156774281f7dd1263241f9ec9ba7d
Lu0804 <abenante.lucia@franchettisalviani.net>
2026-04-10 12:41:16 +0200

    blblblb

---
+++ src/abenantelutzengitasql/LuciMuta.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abenantelutzengitasql;
+
+/**
+ *
+ * @author abenante.lucia
+ */
+public class LuciMuta {
+    /**
+     *  /- GIORNO 1 -\
+     * Qui inizia il nostro pecorso di depressione e yogurt, oggi è stato un giorno lunga e pieno di latte- che per la cronaca fa cagare-  
+     * le tabelle hanno cambiato colore circa 10 volte senza il mio consenso le denuncio per stupro
+     * ora ho voglia di carbonara
+     * di chinese 
+     * e di pollo
+     * serve per la comunicazione, sei stronzo.
+     * ricordiamoci di eliminare questa classe va
+     * hanno troppo tempo libero
+     * vedi gianni che chiede e... questa classe qui?
+     * yougurt ho finito gli altri e c'è a casa deella giu spero MANGIARE
+     * 
+     * 
+     */
+}
